aad7a4fdc647a35c6519d65f292e2076c4836708
Gaurav06120714 <gauravganeshteegulla@gmail.com>
2026-06-09 10:57:17 +0530

    Updated version

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -1,6 +1,3 @@
-// Shared React components for use across Vyro apps.
-// Export reusable components here as they are built.
-
 export { default as Spinner } from './Spinner.js';
 export { StatusBadge } from './StatusBadge.js';
 export { LoadingDots } from './LoadingDots.js';
